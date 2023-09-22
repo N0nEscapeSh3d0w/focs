c31e79cc64f96924db53a356d0fb6d9d0ba7a1bf
EstherWT <71790914+EstherWT@users.noreply.github.com>
2023-09-22 21:28:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,10 @@ custombucket = "semfocs-bucket"
 customregion = "us-east-1"
 
 app = Flask(__name__, static_folder='assets')
-
+#Session
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 bucket = custombucket
 region = customregion
 
