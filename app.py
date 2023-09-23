fc400e0628b0a94373f90f3b83ad7e5aeb3e1845
lamwaihong123 <101498426+lamwaihong123@users.noreply.github.com>
2023-09-23 14:57:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -207,7 +207,7 @@ def displayStaff():
 
 
 
-@app.route('/displayStaff/staffDetails/<string:staff_id>')
+@app.route('/displayStaff/staffDetails/<string:id>')
 def staffDetails(id):
 
     #Get Internship details
