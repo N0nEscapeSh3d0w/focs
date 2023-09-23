3a2ace43ad3bca54a5c434e8a9293fa8c7e306b5
EstherWT <71790914+EstherWT@users.noreply.github.com>
2023-09-23 11:25:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -163,8 +163,8 @@ def enrollDiploma():
     credit_number = 0
     
     for user_grade in grades:
-    if user_grade == 7:
-        credit_number += 1
+        if user_grade == 7:
+            credit_number += 1
 
     if(credit_number < number_of_credit):
         return render_template("enrollFail.html", mismatched = mismatched_subjects, user_credit = credit_number)
